aea85a64c78f34c845d4210730e2faecf048bdc9
sushantgawali <sushant.gawali@gmail.com>
2015-11-27 17:06:55 +0530

    added changes to render yammer widget

--- yammer/views.py
+++ yammer/views.py
@@ -1,11 +1,23 @@
-from django.shortcuts import get_object_or_404, render_to_response
+from edxmako.shortcuts import render_to_response
+from edxmako.paths import add_lookup, lookup_template
 from rest_framework.generics import GenericAPIView
+from opaque_keys.edx.keys import CourseKey
+from courseware.courses import get_course_with_access
 
 
 class YammerView(GenericAPIView):
 
     def get(self, request, course_id):
 
-        print "inside yammer get"
-        context = {}
-        return render_to_response("yammer/yammer.html", context)
+        course_key = CourseKey.from_string(course_id)
+        course = get_course_with_access(request.user, "load", course_key)
+        user = request.user
+        # add_lookup('yammer', 'templates', 'yammer')
+        context = {
+            "course": course,
+            "user_info": {
+                "username": user.username,
+            },
+            "user": user
+        }
+        return render_to_response("yammer/yammer.html", context, {}, "main")
